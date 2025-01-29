c4808a3e6d58e581c34f73669f51bbbdf98d78a1
dominam036 <dominik.namenyi@gmail.com>
2025-01-29 17:54:11 +0100

    fuggveny bevezetese

--- main.js
+++ main.js
@@ -33,50 +33,54 @@ const sorok = [
 const tbl = document.createElement('table'); // Táblázat létrehozása
 document.body.appendChild(tbl); // Táblázat hozzáadása a body-hoz
 
-const thd = document.createElement('thead'); // Fejléc létrehozása
-tbl.appendChild(thd); // Fejléc hozzáadása a táblázathoz
+function RenderTable(){ //RenderTable fuggveny letrehozasa
+    const thd = document.createElement('thead'); // Fejléc létrehozása
+    tbl.appendChild(thd); // Fejléc hozzáadása a táblázathoz
 
-const thr = document.createElement('tr'); // Sor létrehozása a fejlécben
-thd.appendChild(thr); // Sor hozzáadása a fejléchez
+    const thr = document.createElement('tr'); // Sor létrehozása a fejlécben
+    thd.appendChild(thr); // Sor hozzáadása a fejléchez
 
-const th1 = document.createElement('th'); // Első fejléc cella létrehozása
-th1.innerHTML = fejlec.ter; // Cella szövegének beállítása
-thr.appendChild(th1); // Cella hozzáadása a fejlécsorhoz
+    const th1 = document.createElement('th'); // Első fejléc cella létrehozása
+    th1.innerHTML = fejlec.ter; // Cella szövegének beállítása
+    thr.appendChild(th1); // Cella hozzáadása a fejlécsorhoz
 
-const th2 = document.createElement('th'); // Második fejléc cella létrehozása
-th2.innerHTML = fejlec.ido; // Cella szövegének beállítása
-thr.appendChild(th2); // Cella hozzáadása a fejlécsorhoz
+    const th2 = document.createElement('th'); // Második fejléc cella létrehozása
+    th2.innerHTML = fejlec.ido; // Cella szövegének beállítása
+    thr.appendChild(th2); // Cella hozzáadása a fejlécsorhoz
 
-const th3 = document.createElement('th'); // Harmadik fejléc cella létrehozása
-th3.colSpan = 2; // Oszlopok egyesítése
-th3.innerHTML = fejlec.kepv; // Cella szövegének beállítása
-thr.appendChild(th3); // Cella hozzáadása a fejlécsorhoz
+    const th3 = document.createElement('th'); // Harmadik fejléc cella létrehozása
+    th3.colSpan = 2; // Oszlopok egyesítése
+    th3.innerHTML = fejlec.kepv; // Cella szövegének beállítása
+    thr.appendChild(th3); // Cella hozzáadása a fejlécsorhoz
 
-const tbd = document.createElement('tbody'); // Törzs létrehozása
-tbl.appendChild(tbd); // Törzs hozzáadása a táblázathoz
+    const tbd = document.createElement('tbody'); // Törzs létrehozása
+    tbl.appendChild(tbd); // Törzs hozzáadása a táblázathoz
 
-// Sorok hozzáadása a táblázathoz for ciklussal
-for (let i = 0; i < sorok.length; i++) {
-    const sor = sorok[i]; // Aktuális sor
+    // Sorok hozzáadása a táblázathoz for ciklussal
+    for (let i = 0; i < sorok.length; i++) {
+        const sor = sorok[i]; // Aktuális sor
 
-    const tr = document.createElement('tr'); // Sor létrehozása
-    tbd.appendChild(tr); // Sor hozzáadása a törzshöz
+        const tr = document.createElement('tr'); // Sor létrehozása
+        tbd.appendChild(tr); // Sor hozzáadása a törzshöz
 
-    const td1 = document.createElement('td'); // Első cella létrehozása
-    td1.innerHTML = sor.ter; // Cella szövegének beállítása
-    tr.appendChild(td1); // Cella hozzáadása a sorhoz
+        const td1 = document.createElement('td'); // Első cella létrehozása
+        td1.innerHTML = sor.ter; // Cella szövegének beállítása
+        tr.appendChild(td1); // Cella hozzáadása a sorhoz
 
-    const td2 = document.createElement('td'); // Második cella létrehozása
-    td2.innerHTML = sor.ido; // Cella szövegének beállítása
-    tr.appendChild(td2); // Cella hozzáadása a sorhoz
+        const td2 = document.createElement('td'); // Második cella létrehozása
+        td2.innerHTML = sor.ido; // Cella szövegének beállítása
+        tr.appendChild(td2); // Cella hozzáadása a sorhoz
 
-    const td3 = document.createElement('td'); // Harmadik cella létrehozása
-    td3.innerHTML = sor.kepv; // Cella szövegének beállítása
-    tr.appendChild(td3); // Cella hozzáadása a sorhoz
+        const td3 = document.createElement('td'); // Harmadik cella létrehozása
+        td3.innerHTML = sor.kepv; // Cella szövegének beállítása
+        tr.appendChild(td3); // Cella hozzáadása a sorhoz
 
-    if(sor.kepv2){ // Ha van kepv2 akkor megyünk be
-        const td4 = document.createElement('td'); // Negyedik cella létrehozása
-        td4.innerHTML = sor.kepv2; // Cella szövegének beállítása
-        tr.appendChild(td4); // Cella hozzáadása a sorhoz
+        if(sor.kepv2){ // Ha van kepv2 akkor megyünk be
+            const td4 = document.createElement('td'); // Negyedik cella létrehozása
+            td4.innerHTML = sor.kepv2; // Cella szövegének beállítása
+            tr.appendChild(td4); // Cella hozzáadása a sorhoz
+        }
     }
 }
+
+RenderTable();
